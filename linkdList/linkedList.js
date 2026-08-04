76be6398d15f4631a242e04888343cb407b4c3fe
Ismail M <muhammedismailkt@gmail.com>
2026-08-04 13:33:18 +0530

    linkd list sorting

--- linkdList/linkedList.js
+++ linkdList/linkedList.js
@@ -113,3 +113,49 @@ list.swapByNum()
 list.print()
 list.addSumBetween()
 list.print()
+
+
+
+const li = new List()
+li.insert({ id: 101, name: "Rahul", department: "Engineering", score: 82 })
+li.insert({ id: 105, name: "David", department: "HR", score: 91 })
+li.insert({ id: 104, name: "Sara", department: "Finance", score: 75 })
+li.insert({ id: 103, name: "John", department: "Engineering", score: 91 })
+li.insert({ id: 102, name: "Anu", department: "HR", score: 91 })
+li.insert({ id: 106, name: "Maya", department: "Engineering", score: 88 })
+
+
+function mergesort(data) {
+    if (!data || !data.next) return data
+    let slow = data
+    let fast = data
+    let prev = null
+    while (fast && fast.next) {
+        prev = slow
+        slow = slow.next
+        fast = fast.next.next
+    }
+    prev.next = null
+    let left = mergesort(data)
+    let right = mergesort(slow)
+    return merge(left, right)
+}
+function merge(lt, rt) {
+    let dummy = new Node(0)
+    let lst = dummy
+    while (lt && rt) {
+        if (lt.value.id > rt.value.id) {
+            lst.next = rt
+            rt = rt.next
+        } else {
+            lst.next = lt
+            lt = lt.next
+        }
+        lst = lst.next
+    }
+    lst.next = lt ? lt : rt
+    return dummy.next
+}
+
+li.head = mergesort(li.head)
+li.print()
