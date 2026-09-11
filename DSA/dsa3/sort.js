153f59323da1bb5790562edf6bd72ae0f31f189a
Ismail M <muhammedismailkt@gmail.com>
2026-09-11 19:30:00 +0530

    refactor component logic

--- DSA/dsa3/sort.js
+++ DSA/dsa3/sort.js
@@ -44,7 +44,7 @@ function sorting(arr) {
         j++
     }
     return arr 
-}
+} 
 let arr = [90, 8, 5, 23, 56, 89, 4, 56, 8, 32, 14, 98, 37, 18.9, 8, 5, 6, 1]
 // consoleing the task
 console.log(sorting(arr)) 
