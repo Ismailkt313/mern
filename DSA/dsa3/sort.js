1a78e66094cc866d327cd57e6656531b1d475958
Ismail M <muhammedismailkt@gmail.com>
2026-09-12 21:30:00 +0530

    improve application performance

--- DSA/dsa3/sort.js
+++ DSA/dsa3/sort.js
@@ -38,7 +38,7 @@ function sorting(arr) {
         sortArr.push(arr[i])
     }
     let ans = mergeSort(sortArr)
-    let j = 0
+    let j = 0 
     for (let i = 1; i < arr.length; i+=2){
         arr[i] = ans[j] 
         j++
