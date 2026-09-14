f42a314e43d4d3c5aa63f11700eb5ba5d4ded5a8
Ismail M <muhammedismailkt@gmail.com>
2026-09-14 10:20:00 +0530

    improve application logic

--- DSA/dsa3/graph.js
+++ DSA/dsa3/graph.js
@@ -12,7 +12,7 @@ class graph {
     addEdge(v1, v2) { 
         let obj = this.adjList 
         if (!obj[v1]) { 
-            this.addvertex(v1)
+            this.addvertex(v1) 
         }
         if (!obj[v2]) {
             this.addvertex(v2)
